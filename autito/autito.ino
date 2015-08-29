6dd2c43ad1121efa82e0feb47cdd7f245bceef5e
Misael.K <misael_k@yahoo.com.ar>
2015-08-29 11:31:56 -0300

    Cambios de hace 2 horas.

--- autito/autito.ino
+++ autito/autito.ino
@@ -24,8 +24,8 @@ const int motorDerDireccionB = 4; // digital out
 const int motorDerVelocidad = 9; // PWM out
 const int sensorGiroRecta = 2; // digital in
 const int sensorPelotas = A3;
-const int usTrigger = 1; // digital out
-const int us1Echo = A4;
+const int usTrigger = A4; 
+const int us1Echo = 1; // digital out
 const int buzzer = 0; // digital out
 
 byte sensoresPiso = 0;
@@ -40,13 +40,17 @@ const int BBB = 7;
 
 const bool ADELANTE = true;
 const bool ATRAS = false;
-const int velocidadMaxima = 150;
+const int velocidadMaxima = 255;
+const int DISTANCIA_FRENADO_FRONTAL = 15;
+const int VALOR_BLANCO = 700;
 
 const int SEGUIR_DERECHO = 0;
 const int CORREGIR_HACIA_IZQUIERDA = 1;
 const int CORREGIR_HACIA_DERECHA = 2;
 int estadoActual = SEGUIR_DERECHO;
-int estadoAnterior = SEGUIR_DERECHO;
+
+long unsigned int ultimoTiempoSensorIzq = 0;
+long unsigned int ultimoTiempoSensorDer = 0;
 
 int valorSensor = 0;
 
@@ -71,11 +75,15 @@ void setup() {
   pinMode(motorDerVelocidad, OUTPUT);
   pinMode(sensorGiroRecta, INPUT);
   pinMode(sensorPelotas, INPUT);
-  pinMode(usTrigger, OUTPUT);
-  pinMode(us1Echo, INPUT);
+  //pinMode(usTrigger, OUTPUT);
+  //pinMode(us1Echo, INPUT);
   pinMode(buzzer, OUTPUT);
 
   servoDescanso();
+
+  motorIzq(0, ADELANTE);
+  motorDer(0, ADELANTE);
+
 
   if (DEBUG) {
     Serial.begin(9600);  
@@ -83,7 +91,19 @@ void setup() {
 }
 
 inline byte leerSensoresLinea() {
-  return digitalRead(sensorIzq) + digitalRead(sensorCen) + digitalRead(sensorDer);
+  int sensor1 = (analogRead(sensorIzq) < VALOR_BLANCO) ? 4 : 0;
+  int sensor2 = (analogRead(sensorCen) < VALOR_BLANCO) ? 2 : 0;
+  int sensor3 = (analogRead(sensorDer) < VALOR_BLANCO) ? 1 : 0;
+  if (sensor1) {
+    ultimoTiempoSensorIzq = millis();
+  }  
+  if (sensor3) {
+    ultimoTiempoSensorDer = millis();
+  }  
+  return sensor1 + sensor2 + sensor3;
+}
+inline int leerSensorGiro() {
+  return ((digitalRead(sensorGiroRecta) == 0) ? 1 : 0);
 }
 
 int obtenerDistanciaFrontal() {
@@ -134,23 +154,41 @@ void empujarPelota() {
   delay(100);
 }
 
+void frenar() {
+  motorIzq(0, ADELANTE);
+  motorDer(0, ADELANTE);
+}
+
 void girarIzquierda() {
   // gira a la izquierda aprox 90 grados,
   // hasta volver a encontrar la pista por delante
-  
-  motorIzq(0, ADELANTE);
-  motorDer(velocidadMaxima, ADELANTE);
-  //espera un poco para que los sensores queden afuera de la pista
-  delay(50);
-  while(1) {
-    sensoresPiso = leerSensoresLinea();
-    // si leo cualquier sensor de adelante, listo
-    if (sensoresPiso) {
-      motorIzq(0, ADELANTE);
-      motorDer(0, ADELANTE);
-      break;
-    }
+  motorIzq(velocidadMaxima, ATRAS);
+  motorDer(velocidadMaxima, ATRAS);
+  delay(5);
+  frenar();
+  delay(1000);
+  while(!leerSensorGiro()){
+    motorIzq(velocidadMaxima, ATRAS);
+    motorDer(velocidadMaxima, ATRAS);
   }
+  
+  sensoresPiso = leerSensoresLinea();
+
+
+  while(sensoresPiso) {
+    sensoresPiso = leerSensoresLinea();
+    motorIzq(velocidadMaxima, ATRAS);
+    motorDer(velocidadMaxima, ADELANTE);
+  }
+  frenar();
+  delay(1000);
+  while(!sensoresPiso) {
+    sensoresPiso = leerSensoresLinea();
+    motorIzq(velocidadMaxima/2, ATRAS);
+    motorDer(velocidadMaxima, ADELANTE);
+  }
+  frenar();
+  delay(1000);
 }
 
 void darVueltaAtras() {
@@ -170,9 +208,8 @@ void avanzarHastaParedDelantera() {
   motorDer(velocidadMaxima, ADELANTE);
   while (1) {
     valorSensor = obtenerDistanciaFrontal();
-    if (valorSensor < 200) {
-      motorIzq(0, ADELANTE);
-      motorDer(0, ADELANTE);
+    if (valorSensor < DISTANCIA_FRENADO_FRONTAL) {
+      frenar();
       break;
     }
   }
@@ -211,6 +248,50 @@ void servoGolpe(void) {
   empujador.write(80);
 }
 
+void encontrarCuarto() {
+  while(1) {
+    seguirLinea();
+    valorSensor = leerSensorGiro();
+    if (valorSensor) {
+      girarIzquierda();
+      estadoActual = SEGUIR_DERECHO;
+      while(leerSensoresLinea() != BBB) seguirLinea();
+      frenar();
+      delay(1000);
+
+      digitalWrite(buzzer, HIGH);
+      int diferencia = ultimoTiempoSensorIzq - ultimoTiempoSensorDer;
+      long int tiempoProporcional = abs(diferencia) * 20000;
+      if (diferencia > 0) {
+        motorIzq(0, ADELANTE);
+        motorDer(velocidadMaxima, ATRAS);
+      } else {
+        motorIzq(velocidadMaxima, ATRAS);
+        motorDer(0, ADELANTE);
+      }
+      delay(tiempoProporcional);
+      frenar();
+      digitalWrite(buzzer, LOW);
+      
+      /*
+      while(leerSensoresLinea() != BBB){ 
+        if (!(leerSensoresLinea() & 0b00000001))
+          motorDer(velocidadMaxima, ATRAS);
+        else
+          motorDer(0, ATRAS);
+          
+        if (!(leerSensoresLinea() & (1<<2)))
+          motorIzq(velocidadMaxima, ATRAS);
+        else
+          motorIzq(0, ATRAS);          
+      }
+      */
+      return;
+    }
+  }
+}
+
+
 void loop() {
 
   /*while(1) {
@@ -227,23 +308,24 @@ void loop() {
     motorDer(255, ATRAS);
     delay(1000);
   }*/
+  
+  // countdown pal arranque
+  digitalWrite(buzzer, HIGH); delay(2000);
+  digitalWrite(buzzer, LOW);  delay(2000);
 
   // INICIO FASE UNO
   // (recorrer el circuito hasta posicionarse en la zona de carga)
   // seguir linea hasta encontrar una recta a la izquierda
-  while (1) {
-    seguirLinea();
-    valorSensor = digitalRead(sensorGiroRecta);
-    if (valorSensor) {
-      girarIzquierda();
-      avanzarHastaParedDelantera();
-      while(1) {
-        // esperar intervención humana, o algo así
-      }
-      // FIN FASE UNO
-      break;
-    }
+  encontrarCuarto();
+  avanzarHastaParedDelantera();
+  while(1) {
+    // esperar intervención humana, o algo así
   }
+  // FIN FASE UNO
+
+
+    
+
 
 
   // INICIO FASE DOS
@@ -255,7 +337,7 @@ void loop() {
     delay(50); // ojímetro
     while(1) {
       seguirLinea();
-      valorSensor = digitalRead(sensorGiroRecta);
+      valorSensor = leerSensorGiro();
       if (valorSensor) {
         girarIzquierda();
         while(1) {
@@ -271,7 +353,7 @@ void loop() {
     // seguir linea hasta encontrar una recta a la izquierda
     while(1) {
       seguirLinea();
-      valorSensor = digitalRead(sensorGiroRecta);
+      valorSensor = leerSensorGiro();
       if (valorSensor) {
         girarIzquierda();
       }
